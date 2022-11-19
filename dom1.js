f0f07c12b3f93afa65a56f0803e0fb1cace1a578
PriyaGunjal <118348552+PriyaGunjal@users.noreply.github.com>
2022-11-19 16:01:18 +0530

    Add files via upload

---
+++ dom1.js
@@ -0,0 +1,37 @@
+//examin document object
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.forms[0]);
+console.log(document.links);
+console.log(document.images);
+
+//get element by id
+
+console.log(document.getElementById('headerTitle'));
+var headerTitle=document.getElementById('header-title');
+console.log(headerTitle);
+headerTitle.textContent='Hello';
+headerTitle.innerText='Goodbye';
+console.log(headerTitle.innerText);
+headerTitle.innerHTML='<h3>Hello</h3>';
+headerTitle.style.borderBottom='solid 3 px #000';
+
+//get element by class name
+
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent='Hello 2';
+items[1].style.fontWeight='bold';
+items[2].style.backgroundColor='green';
+for(var i=0;i<items.length;i++)
+{
+    
+    items[i].style.fontWeight='bold';
+}
+
+
